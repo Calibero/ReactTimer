76c8886d31644c1b3dbbcac6a253675a637fd102
Pavel Boček <paja.bocek@gmail.com>
2016-12-16 15:09:46 +0100

    Initial commit + automatic tests
    add Navigation component, prepare automatic tests

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+  return (
+    <nav className="navbar navbar-inverse container-fluid">
+      <div className="navbar-left">
+        <ul className="nav navbar-nav">
+          <li className="navbar-text">React Timer App</li>
+          <li>
+            <IndexLink to="/" activeClassName="active">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <ul className="nav navbar-nav navbar-right">
+        <li className="navbar-text">
+          Created by Pavel Boček
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+module.exports = Navigation;
