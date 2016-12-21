9caf2c8dfd5c88640785038892ae5c9652b01554
Pavel Boček <paja.bocek@gmail.com>
2016-12-21 12:56:19 +0100

    Created timer component

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -15,7 +15,7 @@ var Controls = React.createClass({
     var renderStartStopButton = () => {
       if (countdownStatus === 'started') {
         return <button onClick={this.onStatusChange('paused')}>Pause</button>;
-      } else if (countdownStatus === 'paused') {
+      } else {
         return <button onClick={this.onStatusChange('started')}>Start</button>
       }
     }
--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,10 +1,54 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
 
 var Timer = React.createClass({
+  getInitialState: function () {
+    return {
+      count: 0,
+      timerStatus: 'stopped'
+    }
+  },
+  componentDidUpdate: function (prevPros, prevState) {
+    if(this.state.timerStatus !== prevState.timerStatus) {
+      switch (this.state.timerStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval (() => {
+      var newCount = this.state.count + 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+    }, 1000);
+  },
+  handleStatusChange: function (newStatus) {
+    this.setState({
+      timerStatus: newStatus
+    })
+  },
+  handleSetCount: function (newCount) {
+    this.setState({
+      count: newCount
+    })
+  },
   render: function () {
+    var {count, timerStatus} = this.state;
     return (
       <div>
-        <p>Timer.jsx</p>
+        <h1>Timer App</h1>
+        <Clock totalSeconds={count} />
+        <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
       </div>
     )
   }
